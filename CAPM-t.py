870e3b38f9798931f987c45ed506d3e4452596d2
Niels <njcvdmolen@hotmail.com>
2018-09-16 15:56:41 +0200

    commits

--- CAPM-t.py
+++ CAPM-t.py
@@ -2,6 +2,222 @@ import readcapm4
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import scipy.optimize as opt
+import scipy.stats as st
+import math
+
+###########################################################
+### vh= _gh_stepsize(vP)
+def _gh_stepsize(vP):
+    """
+    Purpose:
+        Calculate stepsize close (but not too close) to machine precision
+
+    Inputs:
+        vP      1D array of parameters
+
+    Return value:
+        vh      1D array of step sizes
+    """
+    vh = 1e-8*(np.fabs(vP)+1e-8)   # Find stepsize
+    vh= np.maximum(vh, 5e-6)       # Don't go too small
+
+    return vh
+
+
+###########################################################
+### vG= gradient_2sided(fun, vP, *args)
+def gradient_2sided(fun, vP, *args):
+    """
+    Purpose:
+      Compute numerical gradient, using a 2-sided numerical difference
+
+    Author:
+      Charles Bos, following Kevin Sheppard's hessian_2sided, with
+      ideas/constants from Jurgen Doornik's Num1Derivative
+
+    Inputs:
+      fun     function, as used for minimize()
+      vP      1D array of size iP of optimal parameters
+      args    (optional) extra arguments
+
+    Return value:
+      vG      iP vector with gradient
+
+    See also:
+      scipy.optimize.approx_fprime, for forward difference
+    """
+    iP = np.size(vP)
+    vP= vP.reshape(iP)      # Ensure vP is 1D-array
+
+    # f = fun(vP, *args)    # central function value is not needed
+    vh= _gh_stepsize(vP)
+    mh = np.diag(vh)        # Build a diagonal matrix out of h
+
+    fp = np.zeros(iP)
+    fm = np.zeros(iP)
+    for i in range(iP):     # Find f(x+h), f(x-h)
+        fp[i] = fun(vP+mh[i], *args)
+        fm[i] = fun(vP-mh[i], *args)
+
+    vhr = (vP + vh) - vP    # Check for effective stepsize right
+    vhl = vP - (vP - vh)    # Check for effective stepsize left
+    vG= (fp - fm) / (vhr + vhl)  # Get central gradient
+
+    return vG
+
+
+###########################################################
+### mG= jacobian_2sided(fun, vP, *args)
+def jacobian_2sided(fun, vP, *args):
+    """
+    Purpose:
+      Compute numerical jacobian, using a 2-sided numerical difference
+
+    Author:
+      Charles Bos, following Kevin Sheppard's hessian_2sided, with
+      ideas/constants from Jurgen Doornik's Num1Derivative
+
+    Inputs:
+      fun     function, return 1D array of size iN
+      vP      1D array of size iP of optimal parameters
+      args    (optional) extra arguments
+
+    Return value:
+      mG      iN x iP matrix with jacobian
+
+    See also:
+      numdifftools.Jacobian(), for similar output
+    """
+    iP = np.size(vP)
+    vP= vP.reshape(iP)      # Ensure vP is 1D-array
+
+    vF = fun(vP, *args)     # evaluate function, only to get size
+    iN= vF.size
+
+    vh= _gh_stepsize(vP)
+    mh = np.diag(vh)        # Build a diagonal matrix out of h
+
+    mGp = np.zeros((iN, iP))
+    mGm = np.zeros((iN, iP))
+
+    for i in range(iP):     # Find f(x+h), f(x-h)
+        mGp[:,i] = fun(vP+mh[i], *args)
+        mGm[:,i] = fun(vP-mh[i], *args)
+
+    vhr = (vP + vh) - vP    # Check for effective stepsize right
+    vhl = vP - (vP - vh)    # Check for effective stepsize left
+    mG= (mGp - mGm) / (vhr + vhl)  # Get central jacobian
+
+    return mG
+
+
+###########################################################
+### mH= hessian_2sided(fun, vP, *args)
+def hessian_2sided(fun, vP, *args):
+    """
+    Purpose:
+      Compute numerical hessian, using a 2-sided numerical difference
+
+    Author:
+      Kevin Sheppard, adapted by Charles Bos
+
+    Source:
+      https://www.kevinsheppard.com/Python_for_Econometrics
+
+    Inputs:
+      fun     function, as used for minimize()
+      vP      1D array of size iP of optimal parameters
+      args    (optional) extra arguments
+
+    Return value:
+      mH      iP x iP matrix with symmetric hessian
+    """
+    iP = np.size(vP,0)
+    vP= vP.reshape(iP)    # Ensure vP is 1D-array
+
+    f = fun(vP, *args)
+    vh= _gh_stepsize(vP)
+    vPh = vP + vh
+    vh = vPh - vP
+
+    mh = np.diag(vh)            # Build a diagonal matrix out of vh
+
+    fp = np.zeros(iP)
+    fm = np.zeros(iP)
+    for i in range(iP):
+        fp[i] = fun(vP+mh[i], *args)
+        fm[i] = fun(vP-mh[i], *args)
+
+    fpp = np.zeros((iP,iP))
+    fmm = np.zeros((iP,iP))
+    for i in range(iP):
+        for j in range(i,iP):
+            fpp[i,j] = fun(vP + mh[i] + mh[j], *args)
+            fpp[j,i] = fpp[i,j]
+            fmm[i,j] = fun(vP - mh[i] - mh[j], *args)
+            fmm[j,i] = fmm[i,j]
+
+    vh = vh.reshape((iP,1))
+    mhh = vh @ vh.T             # mhh= h h', outer product of h-vector
+
+    mH = np.zeros((iP,iP))
+    for i in range(iP):
+        for j in range(i,iP):
+            mH[i,j] = (fpp[i,j] - fp[i] - fp[j] + f + f - fm[i] - fm[j] + fmm[i,j])/mhh[i,j]/2
+            mH[j,i] = mH[i,j]
+
+    return mH
+
+
+###########################################################
+### mS2= GetCovML(fnNAvgLnL, vP, iN, *args)
+def GetCovML(fnNAvgLnL, vP, iN, *args):
+    """
+    Purpose:
+      Get the covariance matrix of the ML estimation
+
+    Inputs:
+      fnNAvgLnL   function, negative average loglikelihood
+      vP          iP array of parameters
+      iN          integer, number of observations
+      *args       (optional) additional arguments to function
+
+    Return value:
+      mS2         iP x iP covariance matrix
+    """
+    mH= hessian_2sided(fnNAvgLnL, vP, *args)
+    try:
+        mS2= np.linalg.inv(mH)/iN
+    except:
+        print ("Hessian not of full rank...")
+        mS2= np.full(mH.shape, np.nan)
+
+    mS2= (mS2 + mS2.T)/2            # Force mS2 to be symmetric
+
+    return mS2
+
+
+###########################################################
+### vS= GetSDevML(fnNAvgLnL, vP, iN, *args)
+def GetSDevML(fnNAvgLnL, vP, iN, *args):
+    """
+    Purpose:
+      Get the standard deviations of the ML estimation
+
+    Inputs:
+      fnNAvgLnL   function, negative average loglikelihood
+      vP          iP array of parameters
+      iN          integer, number of observations
+      *args       (optional) additional arguments to function
+
+    Return value:
+      vS          iP vector of standard deviations
+    """
+    mS2= GetCovML(fnNAvgLnL, vP, iN, *args)
+    vS= np.sqrt(np.diag(mS2))
+
+    return vS
 
 
 def clean_data(df):
@@ -81,7 +297,7 @@ def init_data(filepath):
 
 def regress_set(df):
     """
-
+    Regress a data set, assuming the first column is a constant
     :param df:
     :return:
     """
@@ -95,7 +311,7 @@ def regress_set(df):
 
 def regress(df):
     """
-    Does regression stuff
+    Performes several regressions
     :return:
     """
     regress_set(df)
@@ -103,15 +319,232 @@ def regress(df):
     for year in df.index.year.unique():
         regress_set(df.loc[df.index.year == year])
 
-
-def optimize(df):
+def GetPars_norm(vP):
     """
-    Compute loglikelihood of regression model
+    Purpose:
+      Read out the parameters from the vector
+
+    Inputs:
+      vP        iK+1 vector with sigma and beta's
+
+    Return value:
+      dS        double, sigma
+      ddf       degrees of freedom
+      vBeta     iK vector, beta's
+    """
+    iK= np.size(vP)-1
+    # Force vP to be a 1D matrix
+    vP= vP.reshape(iK+1,)
+    dS= vP[0]   # np.fabs(vP[0])
+    vBeta= vP[1:]
+
+    return dS, vBeta
+
+def GetPars_student_t(vP):
+    """
+    Purpose:
+      Read out the parameters from the vector
+
+    Inputs:
+      vP        iK+1 vector with sigma and beta's
+
+    Return value:
+      dS        double, sigma
+      ddf       degrees of freedom
+      vBeta     iK vector, beta's
+    """
+    iK= np.size(vP)-1
+    # Force vP to be a 1D matrix
+    vP= vP.reshape(iK+1,)
+    dS= vP[0]   # np.fabs(vP[0])
+    ddf = vP[1]
+    vBeta= vP[2:]
+
+    return dS, ddf, vBeta
+
+
+def LnLRegr_norm(vP, vY, mX):
+    """
+    Purpose:
+        Compute loglikelihood of regression model
+
+    Inputs:
+        vP      iK+1 1D-vector of parameters, with sigma and beta
+        vY      iN 1D-vector of data
+        mX      iN x iK matrix of regressors
+
+    Return value:
+        dLL     double, loglikelihood
+    """
+    (iN, iK)= mX.shape
+    if (np.size(vP) != iK+1):         # Check if vP is as expected
+        print ("Warning: wrong size vP= ", vP)
+
+    (dSigma, vBeta)= GetPars_norm(vP)
+    if (dSigma <= 0):
+        print ("x", end="")
+        return -math.inf
+
+    vE= vY - mX @ vBeta
+
+    vLL= -0.5*(np.log(2*np.pi) + 2*np.log(dSigma) + np.square(vE/dSigma))
+    dLL= np.sum(vLL, axis= 0)
+
+    print (".", end="")             # Give sign of life
+
+    return dLL
+
+
+def EstimateRegr_normal(mX, vY):
+    """
+    Optimize the regression model using
+
+    Adapted from function created by Charles Bos
     :param df:
     :return:
     """
 
+    """
+        Purpose:
+          Estimate the regression model
 
+        Inputs:
+          vY        iN vector of data
+          mX        iN x iK matrix of regressors
+
+        Return value:
+          vP        iK+1 vector of optimal parameters sigma and beta's
+          vS        iK+1 vector of standard deviations
+          dLL       double, loglikelihood
+          sMess     string, output of optimization
+        """
+
+    (iN, iK) = mX.shape
+    vP0 = np.ones(iK + 1)  # Get (bad...) starting values
+
+    # vB= np.linalg.lstsq(mX, vY)[0]
+    # vP0= np.vstack([[[1]], vB])
+
+    dLL = LnLRegr_norm(vP0, vY, mX)
+    print("Initial LL= ", dLL, "\nvP0=", vP0)
+
+    # Create lambda function returning NEGATIVE AVERAGE LL, as function of vP only
+    AvgNLnLRegr = lambda vP: -LnLRegr_norm(vP, vY, mX) / iN
+    # Create function returning NEGATIVE average LL, as function of vP, vY, mX
+    # AvgNLnLRegrXY= lambda vP, vY, mX: -LnLRegr(vP, vY, mX)/iN
+
+    bounds = [(0, None)]
+    for i in range(iK):
+        bounds.append((None, None))
+
+    res = opt.minimize(AvgNLnLRegr, vP0, args=(), method="L-BFGS-B", bounds=bounds)
+    # res= opt.minimize(AvgNLnLRegrXY, vP0, args=(vY, mX), method="BFGS")
+
+    vP = res.x
+    sMess = res.message
+    dLL = -iN * res.fun
+    print("\nBFGS results in ", sMess, "\nPars: ", vP, "\nLL= ", dLL, ", f-eval= ", res.nfev)
+
+    mS2 = GetCovML(AvgNLnLRegr, vP, iN)
+    vS = np.sqrt(np.diag(mS2))
+
+    return vP, vS, dLL, sMess, mS2
+
+
+def LnLRegr_student_t(vP, vY, mX):
+    """
+    Purpose:
+        Compute loglikelihood of regression model
+
+    Inputs:
+        vP      iK+1 1D-vector of parameters, with sigma and beta
+        vY      iN 1D-vector of data
+        mX      iN x iK matrix of regressors
+
+    Return value:
+        dLL     double, loglikelihood
+    """
+    (iN, iK)= mX.shape
+    if (np.size(vP) != iK+2):         # Check if vP is as expected
+        print ("Warning: wrong size vP= ", vP)
+
+    (dSigma, dDegFree, vBeta) = GetPars_student_t(vP)
+    if (dSigma <= 0):
+        print ("x", end="")
+        return -math.inf
+
+    if (dDegFree < 3):
+        print("x", end="")
+        return -math.inf
+
+    vE= vY - mX @ vBeta
+
+    vLL = st.t.logpdf(x=vE, df=dDegFree, loc=np.sqrt(dSigma))
+    dLL= np.sum(vLL, axis= 0)
+
+    print(".", end="")             # Give sign of life
+
+    return dLL
+
+
+def EstimateRegr_student_t(mX, vY):
+    """
+    Purpose:
+      Estimate the regression model
+
+    Inputs:
+      vY        iN vector of data
+      mX        iN x iK matrix of regressors
+
+    Return value:
+      vP        iK+1 vector of optimal parameters sigma and beta's
+      vS        iK+1 vector of standard deviations
+      dLL       double, loglikelihood
+      sMess     string, output of optimization
+    """
+    (iN, iK)= mX.shape
+    vP0= np.ones(iK+2)        # Get (bad...) starting values
+    vP0[1] = 3
+
+    # vB= np.linalg.lstsq(mX, vY)[0]
+    # vP0= np.vstack([[[1]], vB])
+
+    dLL= LnLRegr_student_t(vP0, vY, mX)
+    print("Initial LL= ", dLL, "\nvP0=", vP0)
+
+    # Create lambda function returning NEGATIVE AVERAGE LL, as function of vP only
+    AvgNLnLRegr= lambda vP: -LnLRegr_student_t(vP, vY, mX)/iN
+    # Create function returning NEGATIVE average LL, as function of vP, vY, mX
+    # AvgNLnLRegrXY= lambda vP, vY, mX: -LnLRegr(vP, vY, mX)/iN
+
+    bounds = [(0, None), (3, None)]
+    for i in range(iK):
+        bounds.append((None, None))
+
+    res= opt.minimize(AvgNLnLRegr, vP0, args=(), method="L-BFGS-B", bounds=bounds)
+    # res= opt.minimize(AvgNLnLRegrXY, vP0, args=(vY, mX), method="BFGS")
+    vP= res.x
+    sMess= res.message
+    dLL= -iN*res.fun
+    #print ("\nBFGS results in ", sMess, "\nPars: ", vP, "\nLL= ", dLL, ", f-eval= ", res.nfev)
+
+    mS2= GetCovML(AvgNLnLRegr, vP, iN)
+    vS= np.sqrt(np.diag(mS2))
+
+    return vP, vS, dLL, sMess
+
+def optimize(df):
+    """
+    Performs the ML optimization
+    :param: df a dataframe with sp500 and ibm data
+    :return:
+    """
+    mX = sm.add_constant(df["sp500"])
+    vY = df["ibm"]
+    print('Optimizing with normal')
+    EstimateRegr_normal(mX, vY)
+    print('Optimizing with student t')
+    EstimateRegr_student_t(mX, vY)
 
 def main():
     #Magic 'Numbers'
@@ -119,7 +552,10 @@ def main():
 
     #init
     e_returns_df = init_data(filepath)
+
+    #output
     regress(e_returns_df)
+    optimize(e_returns_df)
 
 
 if __name__ == "__main__":
